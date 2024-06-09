7e18ee5c445384968fd17cc8e0fa588df7d9b369
Meirdavv <meirbusinessac@gmail.com>
2024-06-09 15:54:01 +0300

    finished daily challenge

---
+++ Week02/Day1/DailyChallenge/daily_challenge.py
@@ -0,0 +1,56 @@
+'''
+Using the input function, ask the user for a string. The string must be 10 characters long.
+If it’s less than 10 characters, print a message which states “string not long enough”.
+If it’s more than 10 characters, print a message which states “string too long”.
+If it’s 10 characters, print a message which states “perfect string” and continue the exercise.
+
+Then, print the first and last characters of the given text.
+
+Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+The user enters "HelloWorld"
+Then, you have to construct the string character by character
+H
+He
+Hel
+... etc
+HelloWorld
+
+
+4. Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method). For example:
+
+Hlrolelwod
+'''
+
+import random
+
+
+#exercise1
+size_requirement = 10
+message_for_input = 'Please provide a string, it must be 10 characters long: '
+string = input(message_for_input)
+while (len(string) != size_requirement):
+    size = len(string)
+    if (size < size_requirement):
+        print("String not long enough")
+    if (size > size_requirement):
+        print("String too long")
+    string = input(message_for_input)
+
+print('perfect string')
+
+#exercise2
+print(f"first character: {string[0]}")
+print(f"last character: {string[-1]}")
+
+#exercise3
+string_length = len(string)
+for end_index in range(1, string_length + 1):
+    for start_index in range(end_index):
+        print (string[start_index], end='')
+    print()
+
+#exercise4
+lst = list(string)
+random.shuffle(lst)
+shuffled_string = ''.join(lst)
+print(f"Shuffled string: {shuffled_string}")
