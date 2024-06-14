9a90126d412824376386b45335c6f57785965c20
Meirdavv <meirbusinessac@gmail.com>
2024-06-14 11:49:22 +0300

    started tictactoe

---
+++ Week02/Day5/test.py
@@ -0,0 +1,9 @@
+players_input = [[None]*3]*3
+
+print (players_input)
+
+
+for i in players_input:
+    for j in i:
+        print(j, end=' ')
+    print()
---
+++ Week02/Day5/tic-tac-toe.py
@@ -0,0 +1,23 @@
+def welcome(): #Display welcome message
+    pass
+
+def display_board(players_input): #Display playing board. Inputs- player_input to draw the players input on the board
+    pass
+
+def player_input(players_input): #Receive player input, and add it to players_input which is a 2 dimensional list
+    pass
+
+def check_win(players_input):
+    pass
+
+def play():
+    welcome()
+    players_input = [[None,None,None]*3
+                     ]
+    while True():
+        
+        [
+            [a,b,c]
+            [a,b,c]
+            [a,b,c]
+        ]
