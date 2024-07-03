586cf20dcc7410ad99455ba311ff99e1d7bd7a20
Meirdavv <meirbusinessac@gmail.com>
2024-07-03 18:20:37 +0300

    finished ex3

--- Week05/Day3/exerciseXP/Ex3/script.js
+++ Week05/Day3/exerciseXP/Ex3/script.js
@@ -1,13 +1,23 @@
-// Using a DOM property, retrieve the h1 and console.log it.
-
-// Using DOM methods, remove the last paragraph in the <article> tag.
-
-// Add a event listener which will change the background color of the h2 to red, when it’s clicked on.
-
-// Add an event listener which will hide the h3 when it’s clicked on (use the display:none property).
-
-// Add a <button> to the HTML file, that when clicked on, should make the text of all the paragraphs, bold.
-
-// BONUS : When you hover on the h1, set the font size to a random pixel size between 0 to 100.(Check out this documentation)
-
-// BONUS : When you hover on the 2nd paragraph, it should fade out (Check out “fade css animation” on Google)
+// Declare a global variable named allBoldItems.
+let allBoldItems;
+// Create a function called getBoldItems() that takes no parameter. This function should collect all the bold items inside the paragraph and assign them to the allBoldItems variable.
+function getBoldItems(){
+    allBoldItems = document.getElementsByTagName('strong');
+}
+// Create a function called highlight() that changes the color of all the bold text to blue.
+function highlight(){
+    getBoldItems();
+    for (let boldItem of allBoldItems){
+        boldItem.style.color = "blue";
+    }
+}
+// Create a function called returnItemsToDefault() that changes the color of all the bold text back to black.
+function returnItemsToDefault(){
+    for (let boldItem of allBoldItems){
+        boldItem.style.color = "black";
+    }
+}
+// Call the function highlight() on mouseover (ie. when the mouse pointer is moved onto the paragraph), and the function returnItemsToDefault() on mouseout (ie. when the mouse pointer is moved out of the paragraph).
+pElement = document.getElementsByTagName('p')[0];
+pElement.addEventListener("mouseover",highlight);
+pElement.addEventListener("mouseout",returnItemsToDefault);
