ebb370623e74ce86ee881aa4027d12ae39a4663f
Meirdavv <meirbusinessac@gmail.com>
2024-07-06 19:15:58 +0300

    finished bonuses

--- Week05/Day5/DailyChallenge-ToDoList/script.js
+++ Week05/Day5/DailyChallenge-ToDoList/script.js
@@ -5,9 +5,13 @@ const listTasks = document.getElementsByClassName("listTasks")[0];
 
 listTasks.appendChild(ulElement);
 
+createClearButton();
+
 
 const form = document.getElementById('To-Do-form');
 form.addEventListener("submit",addTask);
+
+let taskId = 0;
 
 function addTask(e){
     e.preventDefault();
@@ -17,7 +21,16 @@ function addTask(e){
     const taskName = formData.get('taskName');
 
     if (taskName !==undefined && taskName!=="")
-        tasks.push(taskName);
+        {
+            let task ={
+                task_id : taskId,
+                text : taskName,
+                done : false
+            }
+            taskId++;
+            tasks.push(task)
+        }
+        
     renderTasks(tasks);
     form.reset();
 }
@@ -25,18 +38,34 @@ function addTask(e){
 function renderTasks(tasks){
     clearTasks();
     for (let task of tasks){
-        taskName = task
+        taskName = task['text'];
+
+        if (taskName == "")
+            continue;
+
 
         let liElement = document.createElement("li");
 
         let iconElement = document.createElement("i");
         iconElement.classList.add("fa=solid","fa-x")
+        iconElement.setAttribute("id", "deleteItem");
+        iconElement.addEventListener('click',function(event){
+            deleteItem(event.target);
+        });
 
         let checkBoxElement = document.createElement("input")
         checkBoxElement.setAttribute("type","checkbox")
+        checkBoxElement.addEventListener("click", function(event){
+            crosslineItem(event.target);
+        });
 
         let taskElement = document.createElement("span");
         taskElement.innerText = taskName;
+        taskElement.setAttribute("data-task-id", task['task_id'])
+
+        if (task['done']=== true){
+            taskElement.classList.add('done');
+        }
 
         liElement.appendChild(iconElement);
         liElement.appendChild(checkBoxElement);
@@ -44,8 +73,39 @@ function renderTasks(tasks){
 
         ulElement.appendChild(liElement);
     }
+
 }
 
 function clearTasks(){
     ulElement.innerHTML ="";
+}
+
+function crosslineItem(checkbox){
+    let taskElement = checkbox.nextElementSibling;
+    item_id = taskElement.getAttribute('data-task-id')
+    if(tasks[item_id].done === true)
+        tasks[item_id].done = false;
+    else{
+        tasks[item_id].done = true
+    }
+    renderTasks(tasks);
+}
+
+
+function deleteItem(icon){
+    let taskElement = icon.nextElementSibling.nextElementSibling;
+    item_id = taskElement.getAttribute('data-task-id')
+    tasks[item_id].text = "";
+    
+    renderTasks(tasks);
+}
+
+function createClearButton(){
+    clearButton = document.createElement("button");
+    clearButton.innerText = 'Clear';
+    clearButton.setAttribute("id", "clearButton");
+    listTasks.appendChild(clearButton);
+    clearButton.addEventListener('click',clearTasks);
+
+    renderTasks(tasks);
 }
