55a7ca4713f8e3e341f53563e23acaefb978bce6
Meirdavv <meirbusinessac@gmail.com>
2024-07-11 18:01:16 +0300

    finished exerciseXP

---
+++ Week06/Day5/exerciseXP/script.js
@@ -0,0 +1,104 @@
+const robots = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+      image: 'https://robohash.org/1?200x200'
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+      image: 'https://robohash.org/2?200x200'
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+      image: 'https://robohash.org/3?200x200'
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+      image: 'https://robohash.org/4?200x200'
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+      image: 'https://robohash.org/5?200x200'
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+      image: 'https://robohash.org/6?200x200'
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+      image: 'https://robohash.org/7?200x200'
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+      image: 'https://robohash.org/8?200x200'
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+      image:'https://robohash.org/9?200x200'
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+      image:'https://robohash.org/10?200x200'
+    }
+    ];
+
+const cardsList = document.getElementById("cardsList");
+for (let robot of robots){
+    const card = document.createElement("div");
+    card.setAttribute("id","card");
+    const cardImg = document.createElement("img");
+    cardImg.setAttribute("src",robot.image);
+    const cardName = document.createElement("h2");
+    cardName.innerText = robot.name;
+    const cardEmail = document.createElement("h4");
+    cardEmail.innerText = robot.email;
+    card.appendChild(cardImg);
+    card.appendChild(cardName);
+    card.appendChild(cardEmail);
+    cardsList.appendChild(card);
+}
+
+
+const searchFilter = document.getElementById("filter");
+searchFilter.addEventListener("keyup", searchRobots);
+
+function searchRobots(){
+    const input = searchFilter.value;
+    
+    for (let card of cardsList.children){
+        cardTitle = card.getElementsByTagName('h2')[0].innerText;
+        if(!cardTitle.toUpperCase().includes(input.toUpperCase()))
+            card.style.display ="none";
+        else
+        card.style.display = "block";
+    }
+}
